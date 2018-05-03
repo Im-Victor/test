61e8f55c5a5c4c00e27f5ed091c90fe59f50f4d2
wangqian <fastid@gmail.com>
2018-05-03 15:35:19 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+function Timer() {
+  this.s1 = 0;
+  this.s2 = 0;
+  // 箭头函数
+  setInterval(() => {
+    console.log("====>", this);
+    this.s1++;
+  }, 1000);
+  // 普通函数
+  setInterval(function() {
+    console.log("function", this);
+    this.s2++;
+  }, 1000);
+}
+
+var timer = new Timer();
+
+//setTimeout(() => console.log("s1: ", timer.s1), 3100);
+//setTimeout(() => console.log("s2: ", timer.s2), 3100);
